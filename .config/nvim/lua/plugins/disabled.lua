1afa96234d989235fe0f92f127a2acbac55b3644
Игорь Алексеевич Поташев <ipotashev@niac-ipotashev.ald.adm-nao.ru>
2024-09-19 11:04:44 +0300

    clean lazyvim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,6 @@ return {
     { "nvimdev/dashboard-nvim", enabled = false },
     { "MagicDuck/grug-far.nvim", enabled = false },
     { "lewis6991/gitsigns.nvim", enabled = false },
+    { "nvim-lualine/lualine.nvim", enabled = false },
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
