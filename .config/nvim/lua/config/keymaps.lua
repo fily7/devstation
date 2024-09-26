3935745ac341929d1ddeda3a5b41bc0f742fb380
Игорь Алексеевич Поташев <ipotashev@niac-ipotashev.ald.adm-nao.ru>
2024-09-26 16:00:42 +0300

    lazyvim -> kickstart

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local map = vim.keymap.set
-
-map({ "i", "n", "v" }, "<C-a>", "<esc>gg0vG$", { desc = "Select all" })
